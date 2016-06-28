d1759ac0dbfcb3f4f6277bba66a2d881245d8117
AdrianMSS <sesamaua@gmail.com>
2016-06-28 14:42:27 -0600

    Commit #12 Mqtt 6

--- app.js
+++ app.js
@@ -19,22 +19,24 @@ var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
 client.on('connect', function() { // When connected
 
   // subscribe to a topic
-  client.subscribe('imaginexyz/world', function() {
+  client.subscribe('imaginexyz/connected', function() {
     // when a message arrives, do something with it
     client.on('message', function(topic, message, packet) {
-    	lastOne['topic'] = topic;
-    	lastOne['message'] = message;
-    	lastOne['packet'] = packet;
-    	lastOne['all'] = "Received '" + message + "' on '" + topic + "'";
       	console.log("Received '" + message + "' on '" + topic + "'");
     });
   });
 
   // publish a message to a topic
-  client.publish('imaginexyz/world', 'my message', function() {
+  client.publish('imaginexyz/connected', 'conectado', function() {
     console.log("Message is published");
-    client.end(); // Close the connection when published
   });
+});
+
+client.on('message', function(topic, message) {  
+  switch (topic) {
+      break;
+      break;
+  }
 });
 
 /**
