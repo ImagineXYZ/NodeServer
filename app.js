79229cb86fd7610fed686eb52104237939b61cef
AdrianMSS <sesamaua@gmail.com>
2016-06-28 11:52:39 -0600

    Commit #5 MQTT Url

--- app.js
+++ app.js
@@ -3,43 +3,6 @@
 * @autor Adrián Sánchez <contact@imaginexyz.com>
 */
 
-//Módulos Necesitados
-var express = require('express'), //Biblioteca para permitir servicios REST
-    cookieParser = require('cookie-parser'), 
-    bodyParser = require('body-parser'); //Biblioteca para manejar los datos de las solicitudes
-
-//REST APIS
-var  database = require('./services/database'); //Archivo donde vamos a comunicarnos con la base de datos
-
-var app = express(); //Instancia de express
-app.use(express.logger('dev')); //Método de ver los mensajes en consola
-app.use(bodyParser());
-
-app.use(express.static(__dirname + '/webpage')); //Página por defecto al ingresar al servidor
-app.use('/imaginexyz', express.static(__dirname + '/graphs')); //Página para vizualizar los datos
-
-//Servicios REST permitidos
-app.get('/imaginexyz/genuinoday', database.getData);  //GET
-app.post('/imaginexyz/genuinodayb', database.newDataBody); //POST Body
-app.post('/imaginexyz/genuinodayq', database.newDataQuery); //POST Query
-app.put('/imaginexyz/genuinoday', database.editData); //PUT
-app.delete('/imaginexyz/genuinoday', database.removeData); //DELETE
-
-app.get('/imaginexyz/graphs', database.getInfo);
-app.get('/imaginexyz/posts', database.getPosts);
-
-//Redirección por defecto
-app.get('*', function (req, res) {
-    res.redirect('../#home', 404);
-});
-
-//Habilitar puerto de escucha para el servidor
-var port = Number(process.env.PORT || 3000);
-app.listen(port);
-console.log('Listening on port ' + port + '...');
-
-
-
 
 var mqtt = require('mqtt'), url = require('url');
 // Parse
@@ -68,3 +31,42 @@ client.on('connect', function() { // When connected
     client.end(); // Close the connection when published
   });
 });
+//Módulos Necesitados
+var express = require('express'), //Biblioteca para permitir servicios REST
+    cookieParser = require('cookie-parser'), 
+    bodyParser = require('body-parser'); //Biblioteca para manejar los datos de las solicitudes
+
+//REST APIS
+var  database = require('./services/database'); //Archivo donde vamos a comunicarnos con la base de datos
+
+var app = express(); //Instancia de express
+app.use(express.logger('dev')); //Método de ver los mensajes en consola
+app.use(bodyParser());
+
+app.use(express.static(__dirname + '/webpage')); //Página por defecto al ingresar al servidor
+app.use('/imaginexyz', express.static(__dirname + '/graphs')); //Página para vizualizar los datos
+
+//Servicios REST permitidos
+app.get('/imaginexyz/genuinoday', database.getData);  //GET
+app.post('/imaginexyz/genuinodayb', database.newDataBody); //POST Body
+app.post('/imaginexyz/genuinodayq', database.newDataQuery); //POST Query
+app.put('/imaginexyz/genuinoday', database.editData); //PUT
+app.delete('/imaginexyz/genuinoday', database.removeData); //DELETE
+
+app.get('/imaginexyz/graphs', database.getInfo);
+app.get('/imaginexyz/posts', database.getPosts);
+
+app.get('/imaginexyz/mqtt', , function (req, res) {
+    res.send(mqtt_url, 404);
+});
+
+//Redirección por defecto
+app.get('*', function (req, res) {
+    res.redirect('../#home', 404);
+});
+
+//Habilitar puerto de escucha para el servidor
+var port = Number(process.env.PORT || 3000);
+app.listen(port);
+console.log('Listening on port ' + port + '...');
+
